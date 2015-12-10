1269131a1da980db034c2f8aaa48bc6c62b1eb74
Roland Züger <rzueger@tocco.ch>
2015-12-10 07:21:27 +0100

    Add props `value` and `onChange` for RadioGroup

--- components/RadioGroup/RadioGroup.js
+++ components/RadioGroup/RadioGroup.js
@@ -6,16 +6,49 @@ class RadioGroup extends Component {
   static propTypes = {
     name: PropTypes.string,
     items: PropTypes.array,
+    value: PropTypes.string,
+    onChange: PropTypes.func,
   };
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: this.props.value || null,
+    };
+  }
 
   render() {
     return (
       <div className="RadioGroup">
         {this.props.items.map(function(item, index) {
-          return <label key={index}><input type="radio" name={this.props.name} value={item.value}/>{item.label}</label>;
+          let input;
+          if (item.value === this.props.value) {
+            input = <input type="radio" name={this.props.name} value={item.value} onChange={this.handleChange.bind(this)} checked="checked"/>;
+          } else {
+            input = <input type="radio" name={this.props.name} value={item.value} onChange={this.handleChange.bind(this)}/>;
+          }
+          return <label key={index}>{input} {item.label}</label>;
         }, this)}
       </div>
     );
+  }
+
+  handleChange(e) {
+    if (e.target.checked === true) {
+      this.setState({
+        value: e.target.value,
+      }, this.fireChangeEvent);
+    }
+  }
+
+  fireChangeEvent() {
+    if (typeof this.props.onChange === 'function') {
+      this.props.onChange({
+        target: {
+          value: this.state.value,
+        },
+      });
+    }
   }
 }
 
