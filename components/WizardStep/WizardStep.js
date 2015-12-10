aed639ee691258692bcac8197e20c9ca9b1d43eb
Roland Züger <rzueger@tocco.ch>
2015-12-10 07:26:54 +0100

    Add `data` state for WizardStep
    - represents the data of this single wizard page
    - is initialized from props or to an empty object
    - must be updated via updateData(key, value)
      - this ensures that the updateData() prop callback
        is called to notify the parent component

--- components/WizardStep/WizardStep.js
+++ components/WizardStep/WizardStep.js
@@ -4,10 +4,29 @@ class WizardStep extends Component {
 
   static propTypes = {
     updateData: PropTypes.func,
+    data: PropTypes.object,
   };
 
-  updateData(data) {
-    this.props.updateData(data);
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: this.props.data || {},
+    };
+  }
+
+  getUpdateHandlerDelegate(key, scope) {
+    return function(e) {
+      scope.updateData(key, e.target.value);
+    };
+  }
+
+  updateData(key, value) {
+    const data = this.state.data;
+    data[key] = value;
+    this.setState({
+      data: data,
+    });
+    this.props.updateData(this.state.data);
   }
 }
 
