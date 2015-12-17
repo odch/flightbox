70e44db93ddc37cb959c9414efd5e3c66d27f3fd
Roland Züger <rzueger@tocco.ch>
2015-12-17 23:26:31 +0100

    Extract wizard functionality from DeparturePage

--- components/DeparturePage/DeparturePage.js
+++ components/DeparturePage/DeparturePage.js
@@ -7,8 +7,7 @@ import PassengerData from '../Departure/PassengerData';
 import DepartureArrivalData from '../Departure/DepartureArrivalData';
 import FlightData from '../Departure/FlightData';
 import Finish from '../Departure/Finish';
-import WizardNavigation from '../WizardNavigation';
-import WizardBreadcrumbs from '../WizardBreadcrumbs';
+import WizardContainer from '../WizardContainer';
 import Firebase from 'firebase';
 
 class DeparturePage extends Component {
@@ -16,10 +15,35 @@ class DeparturePage extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      step: 0,
-      data: {},
-      committed: false,
+      data: {}
     };
+    this.pages = [
+      {
+        id: 'aircraft',
+        component: AircraftData,
+        label: 'Flugzeugdaten'
+      },
+      {
+        id: 'pilot',
+        component: PilotData,
+        label: 'Pilot'
+      },
+      {
+        id: 'passenger',
+        component: PassengerData,
+        label: 'Passagiere'
+      },
+      {
+        id: 'departureArrival',
+        component: DepartureArrivalData,
+        label: 'Start und Ziel'
+      },
+      {
+        id: 'flight',
+        component: FlightData,
+        label: 'Flug'
+      }
+    ];
     this.firebaseCollectionRef = new Firebase('https://mfgt-flights.firebaseio.com/departures/');
     if (this.props.params.key) {
       this.update = true;
@@ -46,38 +70,12 @@ class DeparturePage extends Component {
     this.mounted = false;
   }
 
-  getUpdateHandlerDelegate(key, scope) {
-    return function(data) {
-      scope.updateData(key, data);
-    };
-  }
-
-  updateData(key, value) {
-    const data = this.state.data;
-    data[key] = value;
+  commit(data) {
     this.setState({
-      data: data,
-    });
-  }
-
-  nextStep() {
-    this.setState({
-      step: this.state.step + 1,
-    });
-  }
-
-  previousStep() {
-    this.setState({
-      step: this.state.step - 1,
-    });
-  }
-
-  commit() {
-    this.setState({
-      committed: true,
+      data: data
     }, function() {
       if (this.update === true) {
-        this.firebaseCollectionRef.child(this.props.params.key).set(this.state.data);
+        this.firebaseCollectionRef.child(this.props.params.key).set(data);
       } else {
         this.firebaseCollectionRef.push(this.state.data);
       }
@@ -93,93 +91,13 @@ class DeparturePage extends Component {
   }
 
   render() {
-    let rightComponent;
-
-    if (this.state.committed === true) {
-      rightComponent = (
-        <div className="wrapper">
-          <Finish finish={this.finish.bind(this)}/>
-        </div>);
-    } else {
-      const pages = [
-        {
-          component: <AircraftData data={this.state.data.aircraft} updateData={this.getUpdateHandlerDelegate('aircraft', this)}/>,
-          nextStep: this.nextStep.bind(this),
-          previousStep: this.cancel.bind(this),
-          breadcrumb: {
-            label: 'Flugzeugdaten',
-            handler: function() {
-              this.setState({ step: 0 });
-            }.bind(this),
-          },
-        }, {
-          component: <PilotData data={this.state.data.pilot} updateData={this.getUpdateHandlerDelegate('pilot', this)}/>,
-          nextStep: this.nextStep.bind(this),
-          previousStep: this.previousStep.bind(this),
-          breadcrumb: {
-            label: 'Pilot',
-            handler: function() {
-              this.setState({ step: 1 });
-            }.bind(this),
-          },
-        }, {
-          component: <PassengerData data={this.state.data.passenger} updateData={this.getUpdateHandlerDelegate('passenger', this)}/>,
-          nextStep: this.nextStep.bind(this),
-          previousStep: this.previousStep.bind(this),
-          breadcrumb: {
-            label: 'Passagiere',
-            handler: function() {
-              this.setState({ step: 2 });
-            }.bind(this),
-          },
-        }, {
-          component: <DepartureArrivalData data={this.state.data.departureArrival} updateData={this.getUpdateHandlerDelegate('departureArrival', this)}/>,
-          nextStep: this.nextStep.bind(this),
-          previousStep: this.previousStep.bind(this),
-          breadcrumb: {
-            label: 'Start und Ziel',
-            handler: function() {
-              this.setState({ step: 3 });
-            }.bind(this),
-          },
-        }, {
-          component: <FlightData data={this.state.data.flight} updateData={this.getUpdateHandlerDelegate('flight', this)}/>,
-          nextStep: this.commit.bind(this),
-          nextLabel: 'Speichern',
-          previousStep: this.previousStep.bind(this),
-          breadcrumb: {
-            label: 'Flug',
-            handler: function() {
-              this.setState({ step: 4 });
-            }.bind(this),
-          },
-        },
-      ];
-
-      const page = pages[this.state.step];
-
-      const breadcrumbItems = [{
-        label: 'Abflug',
-      }];
-      pages.forEach(function(page) { breadcrumbItems.push(page.breadcrumb); });
-
-      rightComponent = (
-        <div className="wizard-container">
-          <div className="wrapper">
-            <main>
-              <WizardBreadcrumbs items={breadcrumbItems} activeItem={this.state.step + 1}/>
-              {page.component}
-            </main>
-          </div>
-          <footer>
-            <WizardNavigation cancel={this.cancel.bind(this)}
-                              previousStep={page.previousStep}
-                              nextStep={page.nextStep}
-                              nextLabel={page.nextLabel}/>
-          </footer>
-        </div>);
-    }
-
+    const finishComponent = <Finish finish={this.finish.bind(this)}/>;
+    const rightComponent = <WizardContainer data={this.state.data}
+                                            pages={this.pages}
+                                            finishComponent={finishComponent}
+                                            breadcrumbStart="Abflug"
+                                            cancel={this.cancel.bind(this)}
+                                            commit={this.commit.bind(this)}/>;
     return (
       <VerticalHeaderPage className="DeparturePage" component={rightComponent}/>
     );
---
+++ components/WizardContainer/WizardContainer.js
@@ -0,0 +1,124 @@
+import React, { PropTypes, Component } from 'react';
+import './WizardContainer.scss';
+import WizardNavigation from '../WizardNavigation';
+import WizardBreadcrumbs from '../WizardBreadcrumbs';
+
+class WizardContainer extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      step: 0,
+      data: props.data,
+      committed: false
+    };
+  }
+
+  getUpdateHandlerDelegate(key, scope) {
+    return function(data) {
+      scope.updateData(key, data);
+    };
+  }
+
+  updateData(key, value) {
+    const data = this.state.data;
+    data[key] = value;
+    this.setState({
+      data: data
+    });
+  }
+
+  cancel() {
+    this.props.cancel();
+  }
+
+  nextStep() {
+    if (this.isLast()) {
+      this.commit();
+    } else {
+      this.setState({
+        step: this.state.step + 1
+      });
+    }
+  }
+
+  previousStep() {
+    if (this.isFirst()) {
+      this.cancel();
+    }
+    this.setState({
+      step: this.state.step - 1
+    });
+  }
+
+  commit() {
+    this.setState({
+      committed: true
+    }, function() {
+      this.props.commit(this.state.data);
+    }, this);
+  }
+
+  isFirst() {
+    return this.state.step === 0;
+  }
+
+  isLast() {
+    return this.state.step === (this.props.pages.length - 1);
+  }
+
+  render() {
+    if (this.state.committed === true) {
+      return (
+        <div className="WizardContainer">
+          {this.props.finishComponent}
+        </div>);
+    } else {
+      const page = this.props.pages[this.state.step];
+      const breadcrumbItems = this.buildBreadcrumbItems();
+      const pageComponent = <page.component data={this.state.data[page.id]} updateData={this.getUpdateHandlerDelegate(page.id, this)}/>;
+      const nextLabel = this.isLast() ? 'Speichern' : 'Weiter';
+
+      return (
+        <div className="WizardContainer">
+          <div className="wrapper">
+            <main>
+              <WizardBreadcrumbs items={breadcrumbItems} activeItem={this.state.step + 1}/>
+              {pageComponent}
+            </main>
+          </div>
+          <footer>
+            <WizardNavigation cancel={this.cancel.bind(this)}
+                              previousStep={this.previousStep.bind(this)}
+                              nextStep={this.nextStep.bind(this)}
+                              nextLabel={nextLabel}/>
+          </footer>
+        </div>);
+    }
+  }
+
+  buildBreadcrumbItems() {
+    const breadcrumbItems = [{
+      label: this.props.breadcrumbStart
+    }];
+    this.props.pages.forEach(function(page, index) {
+      breadcrumbItems.push({
+        label: page.label,
+        handler: function() {
+          this.setState({ step: index });
+        }.bind(this)
+      });
+    }, this);
+    return breadcrumbItems;
+  }
+}
+
+WizardContainer.propTypes = {
+  pages: PropTypes.array,
+  finishComponent: PropTypes.element,
+  breadcrumbStart: PropTypes.string,
+  cancel: PropTypes.func,
+  commit: PropTypes.func
+};
+
+export default WizardContainer;
