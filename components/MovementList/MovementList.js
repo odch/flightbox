9354d2f6fda23ba414d3b9b19955a1ff61018d8f
Roland Züger <rzueger@tocco.ch>
2015-12-25 23:13:12 +0100

    Load movements with limit (reload dynamically)
    - Load movements of today initially (might be the most interesting ones)
    - Then initialize continuous loading of movements (limit: 50)
    - Increase limit by 50 every time the user reaches the bottom of the window
    - The 'child_added' event is nice, because new movements appear immediately
      on the screen without manual reload of the list

--- components/MovementList/MovementList.js
+++ components/MovementList/MovementList.js
@@ -2,7 +2,9 @@ import React, { PropTypes, Component } from 'react';
 import Predicates from './Predicates.js';
 import MovementGroup from '../MovementGroup';
 import Firebase from 'firebase';
+import MovementsArray from '../../util/MovementsArray.js';
 import { firebaseToLocal } from '../../util/movements.js';
+import dates from '../../core/dates.js';
 
 /**
  * today
@@ -17,25 +19,84 @@ class MovementList extends Component {
     this.state = {
       movements: [],
     };
-    this.firebaseRef = new Firebase(this.props.firebaseUri);
+    this.increase = 50;
+    this.limit = this.increase;
+    this.childAddedSinceLastIncrease = true;
+
+    this.handleScroll = this.handleScroll.bind(this);
   }
 
   componentWillMount() {
-    this.loadData();
+    this.loadOnceOfToday();
+    window.addEventListener('scroll', this.handleScroll);
   }
 
-  loadData() {
-    this.firebaseRef.orderByChild('negativeTimestamp').once('value', this.onFirebaseValue, this);
+  componentWillUnmount() {
+    this.tearDownFirebase();
+    window.removeEventListener('scroll', this.handleScroll);
   }
 
-  onFirebaseValue(snapshot) {
+  tearDownFirebase() {
+    if (this.firebaseRef) {
+      this.firebaseRef.off('child_added', this.onFirebaseChildAdded, this);
+    }
+  }
+
+  loadOnceOfToday() {
+    new Firebase(this.props.firebaseUri)
+      .orderByChild('dateTime')
+      .startAt(dates.isoStartOfDay(new Date()))
+      .endAt(dates.isoEndOfDay(new Date()))
+      .once('value', this.initialValues, this);
+  }
+
+  loadContinuouslyLimited() {
+    this.tearDownFirebase();
+    this.firebaseRef = new Firebase(this.props.firebaseUri)
+      .orderByChild('negativeTimestamp')
+      .limitToFirst(this.limit);
+    this.firebaseRef.on('child_added', this.onFirebaseChildAdded, this);
+  }
+
+  initialValues(snapshot) {
     const movements = [];
     snapshot.forEach((data) => {
       const movement = firebaseToLocal(data.val());
       movement.key = data.key();
-      movements.push(movement);
+
+      movements.unshift(movement);
     });
-    this.setState({ movements: this.state.movements.concat(movements) });
+    this.setState({
+      movements,
+    }, () => {
+      this.loadContinuouslyLimited();
+    }, this);
+  }
+
+  onFirebaseChildAdded(data) {
+    const addedMovement = firebaseToLocal(data.val());
+    addedMovement.key = data.key();
+
+    const sorted = new MovementsArray(this.state.movements);
+    const inserted = sorted.insert(addedMovement);
+
+    if (inserted) {
+      this.childAddedSinceLastIncrease = true;
+      this.setState({ movements: sorted.array });
+    }
+  }
+
+  handleScroll() {
+    if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight
+      && this.childAddedSinceLastIncrease === true) {
+      this.increaseLimit();
+    }
+  }
+
+  increaseLimit() {
+    this.limit += this.increase;
+    this.childAddedSinceLastIncrease = false;
+    this.loadContinuouslyLimited();
   }
 
   render() {
