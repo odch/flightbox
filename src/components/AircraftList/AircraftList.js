7d718be32e80d9e2c1cff56dcd59c071a667405d
Roland Züger <rzueger@tocco.ch>
2016-01-02 21:17:38 +0100

    Add AircraftList component
    - not in use yet
    - displays aircrafts filtered by immatriculation or type (results combined with OR)

---
+++ src/components/AircraftList/AircraftList.js
@@ -0,0 +1,98 @@
+import React, { PropTypes, Component } from 'react';
+import './AircraftList.scss';
+import SearchTerms from './SearchTerms.js';
+import FirebaseList from './FirebaseList.js';
+import Firebase from 'firebase';
+
+class AircraftList extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      aircrafts: [],
+    };
+    this.firebaseRef = new Firebase('https://mfgt-flights.firebaseio.com/aircrafts');
+    this.firebaseList = new FirebaseList(
+      this.firebaseRef,
+      (aircraft1, aircraft2) => aircraft1.key.localeCompare(aircraft2.key)
+    );
+    this.onSearchResult = this.onSearchResult.bind(this);
+  }
+
+  componentWillMount() {
+    this.firebaseList.addResultCallback(this.onSearchResult);
+    this.initSearch(this.props);
+  }
+
+  componentWillUnmount() {
+    this.firebaseList.removeResultCallback(this.onSearchResult);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    this.initSearch(nextProps);
+  }
+
+  onSearchResult(result) {
+    this.setState({
+      aircrafts: result.data,
+    });
+  }
+
+  initSearch(props) {
+    const terms = new SearchTerms();
+    if (props.immatriculation) {
+      terms.key(props.immatriculation.toUpperCase().trim(), ['HB', 'D', 'OE', 'I', 'F', 'N']);
+    }
+    if (props.aircraftType) {
+      terms.child('type', props.aircraftType.toUpperCase().trim());
+    }
+    this.firebaseList.search(terms);
+  }
+
+  clickHandler(item) {
+    if (typeof this.props.onClick === 'function') {
+      this.props.onClick(item);
+    }
+  }
+
+  render() {
+    let content;
+
+    if (this.state.aircrafts.length > 0) {
+      content = (
+        <ul>
+          {this.state.aircrafts.map((item, index) => {
+            return (
+              <li key={index} onMouseDown={this.clickHandler.bind(this, item)}>
+                <span className="immatriculation">{item.key}</span>
+                <span className="type">{item.value.type}</span>
+              </li>
+            );
+          })}
+        </ul>
+      );
+    } else {
+      content = (
+        <div className="empty-message">
+          <em>Keine Ergebnisse</em>
+          <p>Tippen Sie die ersten Buchstaben der Immatrikulation oder des Typs und wählen
+            Sie das gewünschte Flugzeug anschliessend aus der Liste hier aus.</p>
+        </div>
+      );
+    }
+
+    return (
+      <div className="AircraftList">
+        {content}
+      </div>
+    );
+  }
+}
+
+AircraftList.propTypes = {
+  immatriculation: PropTypes.string,
+  aircraftType: PropTypes.string,
+  onClick: PropTypes.func,
+};
+
+export default AircraftList;
