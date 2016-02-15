54204d55cd666be290649e8c7024bd27c861d3f1
Roland Züger <roland.zueger@gmail.com>
2016-02-15 21:25:24 +0100

    Add validation feature to WizardStep
    - public method #validate() which can be called by parent component
      - returns `true`, if validation was successful and `false`, if there are validation errors
      - sets the validation errors to the state of WizardStep
    - new method getValidationConfig() which can be overriden in subclasses
      - available validation types: `required`, `integer`, `match` and `values`
    - new prop `showValidationErrors` to control whether validation errors in state
      should be displayed to the user right now

--- src/components/WizardStep/WizardStep.js
+++ src/components/WizardStep/WizardStep.js
@@ -7,12 +7,36 @@ class WizardStep extends Component {
     super(props);
     this.state = {
       data: this.props.data || {},
+      validationErrors: [],
     };
+  }
+
+  /**
+   * Call this to trigger validation of data of this step.
+   *
+   * Sets validation errors onto state of this component and returns
+   * `true`, if the validation was successful and `false`, if there
+   * are validation errors.
+   *
+   * @public
+   * @returns {boolean} true, if validation was successful and false,
+   * if there are validation errors.
+   */
+  validate() {
+    const validationErrors = this.getValidationErrors();
+    this.setState({
+      validationErrors,
+    });
+    return validationErrors.length === 0;
   }
 
   componentWillReceiveProps(nextProps) {
     this.setState({
       data: nextProps.data,
+    }, () => {
+      if (this.props.showValidationErrors === true) {
+        this.validate();
+      }
     });
   }
 
@@ -46,12 +70,116 @@ class WizardStep extends Component {
       });
     });
   }
+
+  /**
+   * Override this in your subclass.
+   *
+   * Returns an object with keys of the data fields as keys and the validation
+   * configuration for the field as value. The value should contain a property
+   * `types` which contains the validation strategies and a property `message`
+   * containing the message which should be displayed if one of the validation
+   * types fails.
+   *
+   * Supported validation types:
+   * - required (true|false)
+   * - match (regex)
+   * - integer (true|false)
+   * - values (array of allowed values)
+   *
+   * Example:
+   *
+   * {
+   *  immatriculation: {
+   *    types: {
+   *      required: true,
+   *      match: /^[A-Z0-9]+$/,
+   *    },
+   *    message: 'Insert the immatriculation of the aircraft. ' +
+   *             'May contain upper case letters and digits.',
+   *  },
+   *  flightType: {
+   *    types: {
+   *      required: true,
+   *      values: ['commercial', 'private'],
+   *    },
+   *    message: 'Choose type of flight.',
+   *  }
+   *
+   * @returns {Object} the validation configuration.
+   */
+  getValidationConfig() {
+    return {};
+  }
+
+  getValidationError(key) {
+    if (this.props.showValidationErrors === true) {
+      const error = this.state.validationErrors.find(validationError => validationError.key === key);
+      if (error) {
+        return error.message;
+      }
+    }
+    return null;
+  }
+
+  getValidationErrors() {
+    const validationErrors = [];
+
+    const validationConfig = this.getValidationConfig();
+    for (const key in validationConfig) {
+      if (validationConfig.hasOwnProperty(key)) {
+        let valid = true;
+
+        const dataValue = this.state.data[key];
+
+        const validationTypes = validationConfig[key].types || {};
+
+        for (const typeKey in validationTypes) {
+          if (validationTypes.hasOwnProperty(typeKey)) {
+            switch (typeKey) {
+              case 'required':
+                if (validationTypes[typeKey] === true && !dataValue) {
+                  valid = false;
+                }
+                break;
+              case 'integer':
+                if (validationTypes[typeKey] === true && dataValue !== parseInt(dataValue, 10)) {
+                  valid = false;
+                }
+                break;
+              case 'match':
+                if (!validationTypes[typeKey].test(dataValue)) {
+                  valid = false;
+                }
+                break;
+              case 'values':
+                if (validationTypes[typeKey].indexOf(dataValue) === -1) {
+                  valid = false;
+                }
+                break;
+              default:
+                throw new Error('Unknown validation type: ' + typeKey);
+            }
+          }
+        }
+
+        if (valid === false) {
+          validationErrors.push({
+            key,
+            message: validationConfig[key].message,
+          });
+        }
+      }
+    }
+
+    return validationErrors;
+  }
 }
 
 WizardStep.propTypes = {
   updateData: PropTypes.func,
   onKeyUp: PropTypes.func,
   data: PropTypes.object,
+  showValidationErrors: PropTypes.bool,
 };
 
 export default WizardStep;
