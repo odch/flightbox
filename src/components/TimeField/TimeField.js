7c93481867f7e04a3155da54d23260b36481c7d3
Roland Züger <roland.zueger@gmail.com>
2016-03-04 10:02:17 +0100

    Introduce TimeField component
    - see issue report #32
    - based on react-time-picker (https://github.com/zippyui/react-time-picker)

---
+++ src/components/TimeField/TimeField.js
@@ -0,0 +1,49 @@
+import React, { Component, PropTypes } from 'react';
+import './TimeField.scss';
+import TimePicker from 'react-time-picker';
+
+class TimeField extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: props.value,
+    };
+  }
+
+  componentWillReceiveProps(nextProps) {
+    this.setState({
+      value: nextProps.value,
+    });
+  }
+
+  render() {
+    return (
+      <div className="TimeField">
+        <TimePicker
+          value={this.state.value}
+          defaultValue="00:00"
+          onChange={this.updateValueHandler.bind(this)}
+        />
+      </div>
+    );
+  }
+
+  updateValueHandler(timeString) {
+    this.setState({
+      value: timeString,
+    });
+    if (typeof this.props.onChange === 'function') {
+      this.props.onChange({
+        value: timeString,
+      });
+    }
+  }
+}
+
+TimeField.propTypes = {
+  value: PropTypes.string,
+  onChange: PropTypes.func,
+};
+
+export default TimeField;
