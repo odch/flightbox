876d130bc2c93bc77ccd8e34b8dcdcb97ea0ece4
Roland Züger <roland.zueger@gmail.com>
2016-03-19 19:52:29 +0100

    Display users with lastname first is quick selection list
    - looks better, because the list is ordered by lastname

--- src/components/UserList/UserItem.js
+++ src/components/UserList/UserItem.js
@@ -5,7 +5,7 @@ class UserItem extends Component {
   render() {
     return (
       <div className="UserItem">
-        <span className="name">{this.props.item.value.firstname} {this.props.item.value.lastname}</span>
+        <span className="name">{this.props.item.value.lastname} {this.props.item.value.firstname}</span>
         <span className="memberNr">{this.props.item.value.memberNr}</span>
       </div>
     );
