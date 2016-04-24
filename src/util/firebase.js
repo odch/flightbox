c8d7af26f478c5e5685399fd759bd6c243135638
Roland Züger <roland.zueger@gmail.com>
2016-04-24 15:16:22 +0200

    Make Firebase path parameter optional
    - if not given, '/' is used

--- src/util/firebase.js
+++ src/util/firebase.js
@@ -5,7 +5,7 @@ import auth from './auth.js';
 /**
  * Get a firebase ref.
  *
- * @param path The path of the firebase resource (without domain part).
+ * @param path The path of the firebase resource (without domain part) (if not given, '/' is used).
  * @param token The authentication token to use (if not given, auth#getBestToken() is used).
  * @param callback The function to call after authentication (arguments: error, ref, authData).
  */
@@ -13,6 +13,11 @@ function firebase(path, token, callback) {
   if (!token || typeof token === 'function') {
     callback = token;
     token = auth.getBestToken();
+  }
+
+  if (!path || typeof path === 'function') {
+    callback = path;
+    path = '/';
   }
 
   const ref = new Firebase(Config.firebaseUrl + path);
