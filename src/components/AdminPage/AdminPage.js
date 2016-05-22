3190a416b465328d22c0b4e2c0ae80d423adfa92
Roland Züger <roland.zueger@gmail.com>
2016-05-22 21:51:06 +0200

    Redirect from admin page to start page if user is not admin

--- src/components/AdminPage/AdminPage.js
+++ src/components/AdminPage/AdminPage.js
@@ -14,6 +14,12 @@ import JumpNavigation from '../JumpNavigation';
 
 class AdminPage extends Component {
 
+  componentWillMount() {
+    if (this.props.auth.admin !== true) {
+      this.props.history.push('/');
+    }
+  }
+
   render() {
     const logoImagePath = require('../../resources/mfgt_logo_transp.png');
     return (
@@ -25,32 +31,38 @@ class AdminPage extends Component {
             </a>
           </header>
         </BorderLayoutItem>
-        <BorderLayoutItem region="middle">
-          <JumpNavigation/>
-          <ReportForm/>
-          <LockMovementsForm/>
-          <LabeledBox label="Nachrichten" className="messages">
-            <MessageList firebaseUri="/messages/"/>
-          </LabeledBox>
-          <LabeledBox label="Benutzerliste importieren" className="user-import">
-            <UserImportForm/>
-          </LabeledBox>
-          <LabeledBox label="Flugplatzliste importieren" className="aerodrome-import">
-            <AerodromeImportForm/>
-          </LabeledBox>
-          <LabeledBox label="Flugzeugliste importieren" className="aircraft-import">
-            <AircraftImportForm/>
-          </LabeledBox>
-          <LabeledBox label="Flugzeuge der MFGT" className="mfgt-aircrafts">
-            <FirebaseKeyList path="/settings/aircraftsMFGT"/>
-          </LabeledBox>
-          <LabeledBox label="In Lommis stationierte Flugzeuge (ohne Flugzeuge der MFGT)" className="lszt-aircrafts">
-            <FirebaseKeyList path="/settings/aircraftsLSZT"/>
-          </LabeledBox>
-        </BorderLayoutItem>
+        {this.props.auth.admin === true &&
+          <BorderLayoutItem region="middle">
+            <JumpNavigation/>
+            <ReportForm/>
+            <LockMovementsForm/>
+            <LabeledBox label="Nachrichten" className="messages">
+              <MessageList firebaseUri="/messages/"/>
+            </LabeledBox>
+            <LabeledBox label="Benutzerliste importieren" className="user-import">
+              <UserImportForm/>
+            </LabeledBox>
+            <LabeledBox label="Flugplatzliste importieren" className="aerodrome-import">
+              <AerodromeImportForm/>
+            </LabeledBox>
+            <LabeledBox label="Flugzeugliste importieren" className="aircraft-import">
+              <AircraftImportForm/>
+            </LabeledBox>
+            <LabeledBox label="Flugzeuge der MFGT" className="mfgt-aircrafts">
+              <FirebaseKeyList path="/settings/aircraftsMFGT"/>
+            </LabeledBox>
+            <LabeledBox label="In Lommis stationierte Flugzeuge (ohne Flugzeuge der MFGT)" className="lszt-aircrafts">
+              <FirebaseKeyList path="/settings/aircraftsLSZT"/>
+            </LabeledBox>
+          </BorderLayoutItem>}
       </BorderLayout>
     );
   }
 }
+
+AdminPage.propTypes = {
+  auth: React.PropTypes.object,
+  history: React.PropTypes.object,
+};
 
 export default AdminPage;
