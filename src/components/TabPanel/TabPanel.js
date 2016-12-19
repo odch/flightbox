1faaeb4c8fa7e16f33235d39396e6f4e9d05c267
Roland Züger <roland.zueger@gmail.com>
2016-12-19 20:15:57 +0100

    Migrate `TabPanel` to a `styled component`

--- src/components/TabPanel/TabPanel.js
+++ src/components/TabPanel/TabPanel.js
@@ -1,5 +1,53 @@
 import React, { PropTypes, Component } from 'react';
-import './TabPanel.scss';
+import styled from 'styled-components';
+
+const HeaderLink = styled.a`
+  cursor: pointer;
+  display: block;
+  padding: 1em;
+  ${props => props.active && `color: ${props.theme.colors.main};`}
+  ${props => props.active && `background-color: ${props.theme.colors.background};`}
+  
+  &:hover {
+    background-color: ${props => props.theme.colors.background}
+  }
+`;
+
+const StyledHeaderItem = styled.li`
+  float: left;
+  display: block;
+  width: 50%;
+  text-align: center;
+  font-weight: bold;
+  font-size: 1.5em;
+`;
+
+const HeaderItem = props => (
+  <StyledHeaderItem>
+    <HeaderLink onClick={props.onClick} active={props.active}>{props.label}</HeaderLink>
+  </StyledHeaderItem>
+);
+
+const StyledHeader = styled.div`
+  overflow: hidden;
+  margin-bottom: 2em;
+  border-bottom: 1px solid ${props => props.theme.colors.main};
+`;
+
+const Header = props => (
+  <StyledHeader>
+    <ul>
+      {props.tabs.map((tab, index) => (
+        <HeaderItem
+          key={index}
+          label={tab.label}
+          active={index === props.activeTab}
+          onClick={props.onClick.bind(null, index)}
+        />
+      ))}
+    </ul>
+  </StyledHeader>
+);
 
 class TabPanel extends Component {
 
@@ -14,21 +62,11 @@ class TabPanel extends Component {
     const tab = this.props.tabs[this.state.activeTab];
     return (
       <div className="TabPanel">
-        <div className="header">
-          <ul>
-            {this.props.tabs.map(function(tab, index) {
-              let className = '';
-              if (index === this.state.activeTab) {
-                className += ' active';
-              }
-              return (
-                <li className={className} key={index}>
-                  <a onClick={this.tabClick.bind(this, index)}>{tab.label}</a>
-                </li>
-              );
-            }, this)}
-          </ul>
-        </div>
+        <Header
+          tabs={this.props.tabs}
+          activeTab={this.state.activeTab}
+          onClick={this.tabClick.bind(this)}
+        />
         <div className="content">{tab.component}</div>
       </div>
     );
