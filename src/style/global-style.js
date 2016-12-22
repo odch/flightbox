fbf346688034b1ecd8f2e653521d1d91f3cbda42
Roland Züger <roland.zueger@gmail.com>
2016-12-22 09:09:47 +0100

    Move remaining global styles from `App.scss` to `global-styles.js`

--- src/style/global-style.js
+++ src/style/global-style.js
@@ -1,20 +1,40 @@
 import { injectGlobal } from 'styled-components'
 
-import fontEot from './fonts/MaterialIcons/MaterialIcons-Regular.eot';
-import fontWoff2 from './fonts/MaterialIcons/MaterialIcons-Regular.woff2';
-import fontWoff from './fonts/MaterialIcons/MaterialIcons-Regular.woff';
-import fontTtf from './fonts/MaterialIcons/MaterialIcons-Regular.ttf';
+import iconFontEot from './fonts/MaterialIcons/MaterialIcons-Regular.eot';
+import iconFontWoff2 from './fonts/MaterialIcons/MaterialIcons-Regular.woff2';
+import iconFontWoff from './fonts/MaterialIcons/MaterialIcons-Regular.woff';
+import iconFontTtf from './fonts/MaterialIcons/MaterialIcons-Regular.ttf';
+
+import istokFontTtf from './fonts/IstokWeb/IstokWeb-Regular.ttf';
 
 injectGlobal`
   @font-face {
     font-family: 'Material Icons';
     font-style: normal;
     font-weight: 400;
-    src: url('${fontEot}'); /* For IE6-8 */
+    src: url('${iconFontEot}'); /* For IE6-8 */
     src: local('Material Icons'),
     local('MaterialIcons-Regular'),
-    url('${fontWoff2}') format('woff2'),
-    url('${fontWoff}') format('woff'),
-    url('${fontTtf}') format('truetype');
+    url('${iconFontWoff2}') format('woff2'),
+    url('${iconFontWoff}') format('woff'),
+    url('${iconFontTtf}') format('truetype');
+  }
+  
+  @font-face {
+    font-family: 'istok_webregular';
+    src: url('${istokFontTtf}');
+  }
+  
+  * {
+    font-family: 'istok_webregular', sans-serif;
+  }
+  
+  *:focus {
+    outline: 0;
+  }
+  
+  a {
+    color: #000;
+    text-decoration: none;
   }
 `;
