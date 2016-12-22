1c2e3e0c73bc069b3e2417b77bd0bb2e4341ace2
Roland Züger <roland.zueger@gmail.com>
2016-12-22 21:36:10 +0100

    Add styled component `Centered`
    Centers a div horizontally and vertically.

---
+++ src/components/Centered/Centered.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Centered = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  margin-right: -50%;
+  transform: translate(-50%, -50%);
+`;
+
+export default Centered;
