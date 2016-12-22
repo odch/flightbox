1ba183c71da171764d8f81a62d2bdd0abf0e0e0e
Roland Züger <roland.zueger@gmail.com>
2016-12-22 21:37:10 +0100

    Use `<Centered>` component in <App>
    App.scss is not needed anymore now.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,33 +1,19 @@
-import React, { PropTypes, Component } from 'react';
+import React, {PropTypes, Component} from 'react';
 import LoginPage from '../../containers/LoginPageContainer';
-import './App.scss';
+import Centered from '../Centered';
 import 'moment/locale/de';
 
-
-class App extends Component {
-
-  render() {
-    if (this.props.auth.initialized !== true) {
-      return (
-        <div className="App loading">
-          <div>Bitte warten ...</div>
-        </div>
-      );
-    }
-
-    if (this.props.auth.authenticated !== true || this.props.showLogin === true) {
-      return (
-        <LoginPage/>
-      );
-    }
-
-    return (
-      <div className="App">
-        {this.props.children}
-      </div>
-    );
+const App = props => {
+  if (props.auth.initialized !== true) {
+    return <Centered>Bitte warten ...</Centered>;
   }
-}
+
+  if (props.auth.authenticated !== true || props.showLogin === true) {
+    return <LoginPage/>;
+  }
+
+  return <div>{props.children}</div>;
+};
 
 App.propTypes = {
   children: PropTypes.element.isRequired,
