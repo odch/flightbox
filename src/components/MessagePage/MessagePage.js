fc5ad7f4c0c918ac63e83cb5c5422835bc971ac4
Roland Züger <roland.zueger@gmail.com>
2017-01-08 19:28:30 +0100

    Use VerticalHeaderLayout in message page

--- src/components/MessagePage/MessagePage.js
+++ src/components/MessagePage/MessagePage.js
@@ -1,37 +1,23 @@
 import React, { Component } from 'react';
-import './MessagePage.scss';
-import BorderLayout from '../BorderLayout';
-import BorderLayoutItem from '../BorderLayoutItem';
+import Content from './Content';
+import VerticalHeaderLayout from '../VerticalHeaderLayout';
 import MessageForm from '../MessageForm';
 import JumpNavigation from '../JumpNavigation';
-import Logo from '../Logo';
 
-class MessagePage extends Component {
-
-  render() {
-    return (
-      <BorderLayout className="MessagePage">
-        <BorderLayoutItem region="west">
-          <header>
-            <a href="#/">
-              <Logo className="logo"/>
-            </a>
-          </header>
-        </BorderLayoutItem>
-        <BorderLayoutItem region="middle">
-          <JumpNavigation/>
-          <MessageForm
-            sent={this.props.sent}
-            commitFailed={this.props.commitFailed}
-            onSubmit={this.props.saveMessage}
-            resetMessageForm={this.props.resetMessageForm}
-            confirmSaveMessageSuccess={this.props.confirmSaveMessageSuccess}
-          />
-        </BorderLayoutItem>
-      </BorderLayout>
-    );
-  }
-}
+const MessagePage = props => (
+  <VerticalHeaderLayout>
+    <Content>
+      <JumpNavigation/>
+      <MessageForm
+        sent={props.sent}
+        commitFailed={props.commitFailed}
+        onSubmit={props.saveMessage}
+        resetMessageForm={props.resetMessageForm}
+        confirmSaveMessageSuccess={props.confirmSaveMessageSuccess}
+      />
+    </Content>
+  </VerticalHeaderLayout>
+);
 
 MessagePage.propTypes = {
   sent: React.PropTypes.bool.isRequired,
