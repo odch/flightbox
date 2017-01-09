5bc70f0bbc1987378dbc3b2a0c85c3e7609b17ba
Roland Züger <roland.zueger@gmail.com>
2017-01-09 20:28:20 +0100

    Add project conf and theme for `lszk`

---
+++ theme/lszk/index.js
@@ -0,0 +1,13 @@
+const logo = require('./logo.svg');
+
+const theme = {
+  colors: {
+    main: '#284496',
+    background: '#f6f6f6'
+  },
+  images: {
+    logo
+  }
+};
+
+module.exports = theme;
