65cb7d9f12c532b7fa324dfa498b11eeb93b9f6d
Roland Züger <roland.zueger@gmail.com>
2017-01-13 17:11:26 +0100

    Add color `danger` to themes
    Can be used for error messages or dangerous buttons etc.

--- theme/lszk/index.js
+++ theme/lszk/index.js
@@ -3,7 +3,8 @@ const logo = require('./logo.svg');
 const theme = {
   colors: {
     main: '#284496',
-    background: '#f6f6f6'
+    background: '#f6f6f6',
+    danger: '#c12d19'
   },
   images: {
     logo
--- theme/lszt/index.js
+++ theme/lszt/index.js
@@ -3,7 +3,8 @@ const logo = require('./logo.svg');
 const theme = {
   colors: {
     main: '#ed351c',
-    background: '#f6f6f6'
+    background: '#f6f6f6',
+    danger: '#ed351c'
   },
   images: {
     logo
