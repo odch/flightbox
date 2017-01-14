60e964503dde5952b1b64f0d3b13cc887a8496ba
Roland Züger <roland.zueger@gmail.com>
2017-01-14 12:27:35 +0100

    Port `<Input>` to a styled component

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -1,7 +1,9 @@
 import React from 'react';
-import classNames from 'classnames';
-import './Input.scss';
+import styled from 'styled-components';
 
-const Input = props => <input {...props} className={classNames('Input', props.className)}/>;
+const Input = styled.input`
+  border: 0;
+  border-bottom: 1px solid #000;
+`;
 
 export default Input;
