3c572559d5e495ecb8a306fa1f05c067b9751331
Roland Züger <roland.zueger@gmail.com>
2017-01-28 17:28:51 +0100

    Use project config instead of hardcoded "LSZT" in location confirmation

--- src/components/LocationConfirmationDialog/LocationConfirmationDialog.js
+++ src/components/LocationConfirmationDialog/LocationConfirmationDialog.js
@@ -11,7 +11,7 @@ class LocationConfirmationDialog extends Component {
         <div className="heading">Flugplatz-ICAO-Kürzel nicht gefunden</div>
         <div className="msg">Das von Ihnen eingegebene Flugplatz-ICAO-Kürzel wurde in der Datenbank nicht gefunden.<br/>
           Geben Sie wenn möglich das ICAO-Kürzel und nicht den ausgeschriebenen Namen des Flugplatzes ein.<br/><br/>
-          <b>Lokalflüge:</b> Für Lokalflüge ohne Zwischenlandung geben Sie bitte <b>LSZT</b> ein.<br/><br/>
+          <b>Lokalflüge:</b> Für Lokalflüge ohne Zwischenlandung geben Sie bitte <b>{__CONF__.aerodrome.ICAO}</b> ein.<br/><br/>
           Möchten Sie Ihre Eingabe ändern oder möchten Sie wirklich den eingegebenen Flugplatz verwenden?
         </div>
         <div className="actions">
