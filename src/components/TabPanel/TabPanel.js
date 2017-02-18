c9ae788aa12a177643dcbfd3c72755ab851a74b1
Roland Züger <roland.zueger@gmail.com>
2017-02-18 11:05:12 +0100

    Improve performance of TabPanel
    - Improve performance by using React.PureComponent
    - Pulled components Header and HeaderItems out of TabPanel.js

--- src/components/TabPanel/TabPanel.js
+++ src/components/TabPanel/TabPanel.js
@@ -1,82 +1,43 @@
-import React, { PropTypes, Component } from 'react';
-import styled from 'styled-components';
+import React from 'react';
+import Header from './Header';
 
-const HeaderLink = styled.a`
-  cursor: pointer;
-  display: block;
-  padding: 1em;
-  ${props => props.active && `color: ${props.theme.colors.main};`}
-  ${props => props.active && `background-color: ${props.theme.colors.background};`}
-  
-  &:hover {
-    background-color: ${props => props.theme.colors.background}
-  }
-`;
-
-const StyledHeaderItem = styled.li`
-  float: left;
-  display: block;
-  width: 50%;
-  text-align: center;
-  font-weight: bold;
-  font-size: 1.5em;
-`;
-
-const HeaderItem = props => (
-  <StyledHeaderItem>
-    <HeaderLink onClick={props.onClick} active={props.active}>{props.label}</HeaderLink>
-  </StyledHeaderItem>
-);
-
-const StyledHeader = styled.div`
-  overflow: hidden;
-  margin-bottom: 2em;
-  border-bottom: 1px solid ${props => props.theme.colors.main};
-`;
-
-const Header = props => (
-  <StyledHeader>
-    <ul>
-      {props.tabs.map((tab, index) => (
-        <HeaderItem
-          key={index}
-          label={tab.label}
-          active={index === props.activeTab}
-          onClick={props.onClick.bind(null, index)}
-        />
-      ))}
-    </ul>
-  </StyledHeader>
-);
-
-class TabPanel extends Component {
+class TabPanel extends React.Component {
 
   constructor(props) {
     super(props);
     this.state = {
       activeTab: 0
     };
+    this.handleTabClick = this.handleTabClick.bind(this);
   }
 
   render() {
     const tab = this.props.tabs[this.state.activeTab];
     return (
-      <div className="TabPanel">
+      <div>
         <Header
           tabs={this.props.tabs}
           activeTab={this.state.activeTab}
-          onClick={this.tabClick.bind(this)}
+          onClick={this.handleTabClick}
         />
-        <div className="content">{tab.component}</div>
+        <div>{tab.component}</div>
       </div>
     );
   }
 
-  tabClick(index) {
+  handleTabClick(index) {
     this.setState({
       activeTab: index
     });
   }
 }
+
+TabPanel.propTypes = {
+  tabs: React.PropTypes.arrayOf(
+    React.PropTypes.shape({
+      component: React.PropTypes.element.isRequired
+    })
+  ).isRequired
+};
 
 export default TabPanel;
