a3c9f90c63d2804f0a91b59448445666281333fb
Roland Züger <roland.zueger@gmail.com>
2017-04-06 06:31:18 +0200

    Introduce action `selectMovement` to select a movement in the list
    - Not in use yet
    - Dispatch `selectMovement(movementKey)` to select a certain movement
    - Dispatch `selectMovement(null)` to reset the selection

--- src/modules/ui/movements/reducer.js
+++ src/modules/ui/movements/reducer.js
@@ -13,13 +13,21 @@ function hideDeleteConfirmationDialog(state) {
   });
 }
 
+function selectMovement(state, action) {
+  return Object.assign({}, state, {
+    selected: action.payload.key || null,
+  });
+}
+
 const ACTION_HANDLERS = {
   [actions.SHOW_DELETE_CONFIRMATION_DIALOG]: showDeleteConfirmationDialog,
   [actions.HIDE_DELETE_CONFIRMATION_DIALOG]: hideDeleteConfirmationDialog,
+  [actions.SELECT_MOVEMENT]: selectMovement,
 };
 
 const INITIAL_STATE = {
   deleteConfirmation: null,
+  selected: null,
 };
 
 export default reducer(INITIAL_STATE, ACTION_HANDLERS);
