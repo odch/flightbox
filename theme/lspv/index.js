71e60fd52cc915559218f3a8963d2ec0e5b0e072
Roland Züger <roland.zueger@gmail.com>
2017-04-27 21:24:52 +0200

    Add project LSPV
    TODO: Set correct URLs in environments config section

---
+++ theme/lspv/index.js
@@ -0,0 +1,14 @@
+const logo = require('./logo.gif');
+
+const theme = {
+  colors: {
+    main: '#1d77d7',
+    background: '#f6f6f6',
+    danger: '#fa4214'
+  },
+  images: {
+    logo
+  }
+};
+
+module.exports = theme;
