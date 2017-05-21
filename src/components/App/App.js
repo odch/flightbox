3e0c72c56190d0a400a27b6ff07b73c3f23998d1
Roland Züger <roland.zueger@gmail.com>
2017-05-21 13:02:16 +0200

    Display rotating icon on app initialization screen

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,13 +1,14 @@
 import React, {PropTypes} from 'react';
 import LoginPage from '../../containers/LoginPageContainer';
 import Centered from '../Centered';
+import MaterialIcon from '../MaterialIcon';
 
 class App extends React.PureComponent {
 
   render() {
     const props = this.props;
     if (props.auth.initialized !== true) {
-      return <Centered>Bitte warten ...</Centered>;
+      return <Centered><MaterialIcon icon="sync" rotate="left"/> Bitte warten ...</Centered>;
     }
 
     if (props.auth.authenticated !== true || props.showLogin === true) {
