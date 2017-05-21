53e6b5a71ca4bdcd40354f59695d29d0078e9500
Roland Züger <roland.zueger@gmail.com>
2017-05-21 13:03:23 +0200

    Display rotating icon in movements loading indicator

--- src/components/MovementList/LoadingInfo.js
+++ src/components/MovementList/LoadingInfo.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import styled from 'styled-components';
+import MaterialIcon from '../MaterialIcon';
 
 const Wrapper = styled.div`
   text-align: center;
 `;
 
 const LoadingInfo = () => (
-  <Wrapper>Bewegungen werden geladen ...</Wrapper>
+  <Wrapper><MaterialIcon icon="sync" rotate="left"/> Bewegungen werden geladen ...</Wrapper>
 );
 
 export default LoadingInfo;
