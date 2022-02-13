4bb71a44b6a190b498b1cd7fa3a281013b1c9337
Roland Züger <rzueger@tocco.ch>
2022-02-13 13:43:44 +0100

    Add possibility to disable links in UI for certain users
    If the Flightbox runs at the airport on a terminal with a special
    static login it's not desired that anyone can log out or leave
    the browser window.
    
    For these logins, links can be disabled in the database by setting
    `logins[$login_id].links` to `false`.
    
    This hides the "logout" link at the top right and also disables the
    "open digital Flightbox" link at the bottom right.
    
    Note that the admin collection has also been moved to the `logins`
    collection. So, `admins[$login_id]: true` has to be changed to
    `logins[$login_id].admin: true` in the database.
    
    Ticket: https://app.asana.com/0/1201466020311786/1201613928400936

--- src/components/MarketingLink/MarketingLink.js
+++ src/components/MarketingLink/MarketingLink.js
@@ -2,30 +2,46 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import styled from 'styled-components';
 
+import logo from './odch_logo.png';
+
 const Wrapper = styled.div`
   padding: 1em;
   text-align: right;
 `;
 
 const A = styled.a`
-  color: ${props => props.theme.colors.main};
-  text-decoration: underline;
+  color: #666;
+  text-decoration: none;
 `;
+
+const Logo = styled.img`
+  margin-right: 5px;
+  vertical-align: middle;
+  height: 25px;
+`
 
 class MarketingLink extends React.PureComponent {
 
   render() {
     const props = this.props;
+    const text = <>
+      <Logo src={logo}/>
+      <span>Flightbox</span>
+    </>
+    const content = props.linked
+     ? <A href="https://opendigital.ch/flightbox/" target="_blank">{text}</A>
+        : text
     return (
       <Wrapper className={props.className}>
-        <A href="https://opendigital.ch/flightbox/" target="_blank">Flightbox</A> unterstützt von <A href="https://opendigital.ch/" target="_blank">open digital</A>
+        {content}
       </Wrapper>
     );
   }
 }
 
 MarketingLink.propTypes = {
-  className: PropTypes.string
+  className: PropTypes.string,
+  linked: PropTypes.bool
 };
 
 export default MarketingLink;
