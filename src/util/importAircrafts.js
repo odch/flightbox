6c769feebd4587ae4fdf462f75e617b1e0659b0b
Roland Züger <rzueger@tocco.ch>
2022-05-01 13:05:47 +0200

    Take custom aircrafts from settings into account during import
    You can specify some custom aircrafts under /settings/aircrafts/custom
    that should be part of the aircrafts list even if they're not part of
    the CSV that is imported. E.g. that's useful if there are some aircrafts
    home based on your airport that are registered abroad (e.g. aircrafts
    with "N...." registration on an airport in Switzerland, where we usually
    import only the aircrafts from the BAZL register ("HB...").

--- src/util/importAircrafts.js
+++ src/util/importAircrafts.js
@@ -3,6 +3,7 @@ import importCsv from './importCsv.js';
 function importAircrafts(csvString) {
   const options = {
     path: '/aircrafts',
+    additionalEntriesPath: '/settings/aircrafts/custom',
     columns: [
       { csv: 'Registration', isKey: true, isFirebaseKey: true },
       { csv: 'Type', firebase: 'type' },
