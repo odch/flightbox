4833dccca6503d4df169586125c23f9658806b9c
Roland Züger <roland.zueger@gmail.com>
2023-08-06 21:36:43 +0200

    Add triggers which set the `associatedMovement` field
    The field is set/updated on every movement from now on on creation,
    update and deletion of the movement.

--- functions/index.js
+++ functions/index.js
@@ -8,8 +8,15 @@ const auth = require('./auth');
 const api = require('./api');
 const webhook = require('./webhook');
 const setAssociatedMovementsCronJob = require('./associatedMovements/setAssociatedMovementsCronJob');
+const associatedMovementsTriggers = require('./associatedMovements/setAssociatedMovementsTriggers');
 
 exports.auth = auth;
 exports.api = api;
 exports.webhook = webhook;
 exports.setAssociatedMovementsCronJob = setAssociatedMovementsCronJob;
+exports.setAssociatedMovementOnCreatedDeparture = associatedMovementsTriggers.setAssociatedMovementOnCreatedDeparture;
+exports.setAssociatedMovementOnCreatedArrival = associatedMovementsTriggers.setAssociatedMovementOnCreatedArrival;
+exports.setAssociatedMovementOnUpdatedDeparture = associatedMovementsTriggers.setAssociatedMovementOnUpdatedDeparture;
+exports.setAssociatedMovementOnUpdatedArrival = associatedMovementsTriggers.setAssociatedMovementOnUpdatedArrival;
+exports.setAssociatedMovementOnDeletedDeparture = associatedMovementsTriggers.setAssociatedMovementOnDeletedDeparture;
+exports.setAssociatedMovementOnDeletedArrival = associatedMovementsTriggers.setAssociatedMovementOnDeletedArrival;
