ae7657f173bb3eac2fa3b2215bc64fa976828914
Roland Züger <roland.zueger@gmail.com>
2024-12-11 12:52:47 +0100

    Fix deploying of `auth` cloud function if serviceuser conf not set
    Before, this file didn't return resp. export the `basicAuth` function
    if the config properties were not set which led to errors when
    the `auth` function was being deployed

--- functions/api/basicAuth.js
+++ functions/api/basicAuth.js
@@ -2,14 +2,15 @@ const functions = require('firebase-functions')
 
 const config = functions.config();
 
-if (!config.api || !config.api.serviceuser || !config.api.serviceuser.username || !config.api.serviceuser.password) {
-  console.info(
-    "Set configuration properties `api.serviceuser.username` and `api.serviceuser.password` for the API auth"
-  )
-  return
-}
-
 const basicAuth = (req, res, next) => {
+  if (!config.api || !config.api.serviceuser || !config.api.serviceuser.username || !config.api.serviceuser.password) {
+    console.info(
+      "Set configuration properties `api.serviceuser.username` and `api.serviceuser.password` for the API auth"
+    )
+    res.status(401).send('Unauthorized')
+    return
+  }
+
   const authHeader = req.headers.authorization || ''
   const [type, credentials] = authHeader.split(' ')
 
