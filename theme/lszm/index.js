341330e5713eb2ae21c8e89c02a523072d865a91
Roland Züger <roland.zueger@gmail.com>
2024-12-11 13:23:31 +0100

    Initial commit of new LSZM project
    - landing fees are not the final ones, but just some examples
    - Also needs more adjustments, e.g. for the payment methods

---
+++ theme/lszm/index.js
@@ -0,0 +1,15 @@
+const logo = require('./logo.png');
+
+const theme = {
+  colors: {
+    main: '#324158',
+    background: '#f6f6f6',
+    danger: '#fa4214'
+  },
+  images: {
+    logo
+  },
+  logoSize: 60
+};
+
+module.exports = theme;
