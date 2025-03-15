f78bc82665eed11f87c96d74002f93f4d17d0867
Roland Züger <roland.zueger@gmail.com>
2025-03-15 08:57:21 +0100

    Set more modern styling for boxes in admin page

--- src/components/LabeledBox/LabeledBox.js
+++ src/components/LabeledBox/LabeledBox.js
@@ -1,23 +1,24 @@
 import PropTypes from 'prop-types';
-import React, { Component } from 'react';
+import React from 'react';
 import styled from 'styled-components';
 
 const Wrapper = styled.div`
   margin: 0 0 2em 0;
-  border: 1px solid #eee;
-  box-shadow: 3px;
+  border: 1px solid #ddd;
+  box-shadow: 0 -1px 0 rgba(0,0,0,.03), 0 0 2px rgba(0,0,0,.03), 0 2px 4px rgba(0,0,0,.06);
+  background-color: #fefefe;
 `;
 
 const Label = styled.div`
-  border-bottom: 1px solid ${props => props.theme.colors.main};
-  padding: 1em;
-  background-color: ${props => props.theme.colors.background};
+  padding: ${props => typeof props.padding === 'number' ? `${props.padding}px` : '1rem'};
+  font-weight: bold;
+  font-size: 1.5em;
 `;
 
 const Content = styled.div`
-  padding: ${props => typeof props.padding === 'number' ? `${props.padding}px` : '1em'};
+  padding: ${props => typeof props.padding === 'number' ? `${props.padding}px` : '1rem'};
   ${props => props.contentMaxHeight > 0 && `max-height: ${props.contentMaxHeight}px;`}
-  
+
   a {
     text-decoration: underline;
   }
