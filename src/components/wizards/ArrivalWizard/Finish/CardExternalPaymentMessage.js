6a6b8b595de9951d73848333caf227e10a6690d5
Roland Züger <roland.zueger@gmail.com>
2025-09-24 11:06:21 +0200

    Introduce payment method `card_external`
    - This can be used if there is an external card terminal, which is not
      connected to the Flightbox (the user has to enter the amount they want
      to pay and the Flightbox doesn't know about the status of the payment)
    - Will be used for LSPV

---
+++ src/components/wizards/ArrivalWizard/Finish/CardExternalPaymentMessage.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Message from './Message'
+
+const CardExternalPaymentMessage = () => (
+  <Message>
+    Bitte bezahlen Sie die fällige Landetaxe über das Kartenlesegerät.
+  </Message>
+)
+
+export default CardExternalPaymentMessage
