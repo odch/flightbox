55dc8fd4de1469a2dce8f1c9cb7bb7d9175be8a3
Roland Züger <roland.zueger@gmail.com>
2025-11-23 21:32:24 +0100

    Upgrade to latest v9 firebase sdk
    - In v9, firebase sdk introduced the new modular API. In order to be
      able to adjust the code later step by step, we're using the
      `compat` packages for now (see
      https://firebase.google.com/docs/web/modular-upgrade
      for upgrade instructions)
    - `onValueEvent` is not a function anymore, replaced with
      `on('value', ....)'`
    - Changed to webpack config to fix this new issue:
      ```
      ERROR in ./node_modules/@firebase/app/node_modules/idb/build/index.js
      Module parse failed: Unexpected token (90:4)
      You may need an appropriate loader to handle this file type.
      | }
      | replaceTraps((oldTraps) => ({
      |     ...oldTraps,
      |     get: (target, prop, receiver) => getMethod(target, prop) || oldTraps.get(target, prop, receiver),
      |     has: (target, prop) => !!getMethod(target, prop) || oldTraps.has(target, prop),
       @ ./node_modules/@firebase/app/dist/esm/index.esm2017.js 5:0-29
       @ ./node_modules/@firebase/app-compat/dist/esm/index.esm2017.js
       @ ./node_modules/firebase/compat/app/dist/esm/index.esm.js
       @ ./src/util/firebase.js
       @ ./src/modules/users/sagas.js
       ```

--- webpack.config.js
+++ webpack.config.js
@@ -40,7 +40,7 @@ module.exports = {
     rules: [
       {
         test: /\.jsx?$/,
-        exclude: /node_modules/,
+        exclude: /node_modules\/(?!(idb|@firebase)\/).*/,
         use: {
           loader: 'babel-loader',
           options: {
