f400913f17cd7712e445505328ac23aa6e122d2c
Roland Züger <roland.zueger@gmail.com>
2026-02-19 11:30:55 +0100

    Remove local state in <IncrementationField>
    - There was a bug where the field didn't consider new values if the
      value prop changed and the outdated values was displayed
      (noticed when movment wizard was migrated from redux-form to
      react-final-form)
    - We could fix this in the component, but we can as well remove the
      local state, as it only makes everything unnecessarily complicated

--- src/components/IncrementationField/IncrementationField.js
+++ src/components/IncrementationField/IncrementationField.js
@@ -1,60 +1,34 @@
 import PropTypes from 'prop-types';
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import Button from './Button';
 import Value from './Value';
 
 class IncrementationField extends Component {
 
-  constructor(props) {
-    super(props);
-    const value = typeof this.props.value === 'undefined' ? this.props.minValue : this.props.value;
-    if (value < this.props.minValue) {
-      throw new Error('Given value ' + value + ' is lower than min value ' + this.props.minValue);
-    }
-    this.state = {
-      value,
-    };
-  }
-
   render() {
+    const value = typeof this.props.value === 'undefined' ? this.props.minValue : this.props.value;
+
     if (this.props.readOnly === true) {
       return (
         <div>
-          <Value>{this.state.value}</Value>
+          <Value>{value}</Value>
         </div>
       );
     }
 
     return (
       <div>
-        <Button type="button" onClick={this.decrement.bind(this)} data-cy={`${this.props.dataCy}-decrement`}>-</Button>
-        <Value>{this.state.value}</Value>
-        <Button type="button" onClick={this.increment.bind(this)} data-cy={`${this.props.dataCy}-increment`}>+</Button>
+        <Button type="button" onClick={() => this.change(value - 1)} data-cy={`${this.props.dataCy}-decrement`}>-</Button>
+        <Value>{value}</Value>
+        <Button type="button" onClick={() => this.change(value + 1)} data-cy={`${this.props.dataCy}-increment`}>+</Button>
       </div>
     );
   }
 
-  decrement() {
-    let newValue = this.state.value - 1;
+  change(newValue) {
     if (newValue < this.props.minValue) newValue = this.props.minValue;
-    this.setState({
-      value: newValue,
-    }, this.fireChangeEvent.bind(this));
-  }
-
-  increment() {
-    this.setState({
-      value: this.state.value + 1,
-    }, this.fireChangeEvent.bind(this));
-  }
-
-  fireChangeEvent() {
     if (typeof this.props.onChange === 'function') {
-      this.props.onChange({
-        target: {
-          value: this.state.value,
-        },
-      });
+      this.props.onChange({ target: { value: newValue } });
     }
   }
 }
