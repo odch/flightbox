edd52cfc82d94476ad6138fddb40c313358e8cbc
Roland Züger <roland.zueger@gmail.com>
2026-04-20 10:03:20 +0200

    Convert LoginInfo to function component

--- src/components/StartPage/LoginInfo.tsx
+++ src/components/StartPage/LoginInfo.tsx
@@ -1,9 +1,9 @@
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import styled from 'styled-components';
 import MaterialIcon from '../MaterialIcon';
 import {Link} from 'react-router-dom'
-import { withTranslation } from 'react-i18next'
+import { useTranslation } from 'react-i18next'
 
 const StyledWrapper = styled.div`
   position: relative
@@ -84,7 +84,7 @@ const StyledMenu = styled.div`
   box-shadow: rgba(0, 0, 0, 0.1) 0px 1px 3px 0px, rgba(0, 0, 0, 0.06) 0px 1px 2px 0px;
 `
 
-const getUsername = (authData, t) => {
+const getUsername = (authData: any, t: (key: string) => string) => {
   if (authData.email) {
     return authData.email
   }
@@ -97,84 +97,60 @@ const getUsername = (authData, t) => {
   return authData.uid
 }
 
-class LoginInfo extends React.Component<any, any> {
-  menuRef: any;
+const LoginInfo = (props: any) => {
+  const { t } = useTranslation();
+  const [menuOpen, setMenuOpen] = useState(false);
+  const menuRef = useRef<HTMLDivElement | null>(null);
 
-  constructor(props) {
-    super(props)
-
-    this.handleUserNameClick = this.handleUserNameClick.bind(this)
-    this.handleClickOutside = this.handleClickOutside.bind(this)
-    this.setMenuRef = this.setMenuRef.bind(this);
-
-    this.state = {
-      menuOpen: false
-    }
-  }
-
-  handleUserNameClick(e) {
-    e.stopPropagation();
-    this.setState(prevState => ({ menuOpen: !prevState.menuOpen }), () => {
-      if (this.state.menuOpen) {
-        document.addEventListener('click', this.handleClickOutside);
-      } else {
-        document.removeEventListener('click', this.handleClickOutside);
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (menuRef.current && !menuRef.current.contains(event.target as Node)) {
+        setMenuOpen(false);
       }
-    });
-  }
+    };
 
-  handleClickOutside(event) {
-    if (this.menuRef && !this.menuRef.contains(event.target)) {
-      this.setState({ menuOpen: false });
-      document.removeEventListener('click', this.handleClickOutside);
+    if (menuOpen) {
+      document.addEventListener('click', handleClickOutside);
     }
-  }
+    return () => document.removeEventListener('click', handleClickOutside);
+  }, [menuOpen]);
 
-  componentWillUnmount() {
-    document.removeEventListener('click', this.handleClickOutside);
-  }
+  const handleUserNameClick = (e: React.MouseEvent) => {
+    e.stopPropagation();
+    setMenuOpen(prev => !prev);
+  };
 
-  setMenuRef(node) {
-    this.menuRef = node;
-  }
-
-  render() {
-    const props = this.props;
-    const { t } = props;
-
-    if (props.auth.authenticated === true && typeof props.auth.data.uid === 'string') {
-      return (
-        <StyledWrapper className={props.className} data-cy="login-info">
-          <StyledUserNameWrapper onClick={this.handleUserNameClick}>
-            <MaterialIcon icon="account_box"/>
-            <UserName>{getUsername(props.auth.data, t)}</UserName>
-          </StyledUserNameWrapper>
-          {this.state.menuOpen && props.auth.data.links !== false && this.renderMenu()}
-        </StyledWrapper>
-      );
-    }
-
+  const renderMenu = () => {
+    const auth = props.auth.data;
     return (
-      <div className={props.className}>
-        <Button onClick={props.showLogin}>{t('common.login')}</Button>
-      </div>
-    );
-  }
-
-  renderMenu() {
-    const auth = this.props.auth.data;
-    const { t } = this.props;
-    return (
-      <StyledMenu ref={this.setMenuRef}>
+      <StyledMenu ref={menuRef}>
         <StyledMenuUsername>{getUsername(auth, t)}</StyledMenuUsername>
         {(typeof __CONF__ === 'undefined' || __CONF__.profileEnabled !== false) && auth && auth.guest !== true && auth.kiosk !== true && auth.uid !== 'ipauth' && (
           <StyledMenuLink to="/profile" data-cy="profile">{t('login.profile')}</StyledMenuLink>
         )}
-        <StyledMenuButton onClick={this.props.logout} data-cy="logout">{t('login.logout')}</StyledMenuButton>
+        <StyledMenuButton onClick={props.logout} data-cy="logout">{t('login.logout')}</StyledMenuButton>
       </StyledMenu>
-    )
+    );
+  };
+
+  if (props.auth.authenticated === true && typeof props.auth.data.uid === 'string') {
+    return (
+      <StyledWrapper className={props.className} data-cy="login-info">
+        <StyledUserNameWrapper onClick={handleUserNameClick}>
+          <MaterialIcon icon="account_box"/>
+          <UserName>{getUsername(props.auth.data, t)}</UserName>
+        </StyledUserNameWrapper>
+        {menuOpen && props.auth.data.links !== false && renderMenu()}
+      </StyledWrapper>
+    );
   }
-}
+
+  return (
+    <div className={props.className}>
+      <Button onClick={props.showLogin}>{t('common.login')}</Button>
+    </div>
+  );
+};
 
 (LoginInfo as any).propTypes = {
   className: PropTypes.string,
@@ -189,4 +165,4 @@ class LoginInfo extends React.Component<any, any> {
   showLogin: PropTypes.func.isRequired,
 };
 
-export default withTranslation()(LoginInfo);
+export default LoginInfo;
